481e081b92310ede4b59eafa8df9aaa28cb2ff76
rhazen35 <rhazen35@gmail.com>
2016-09-20 13:47:17 +0200

    Start of cleaning/optimizing code

--- app/core/Configuration.php
+++ app/core/Configuration.php
@@ -8,28 +8,22 @@
 
 namespace app\core;
 
+if( !class_exists( "Configuration" ) ):
 
-class Configuration
-{
-
-    public static function dbCredentials()
+    class Configuration
     {
-        $dbArray = array(
-            "dbhost" => '127.0.0.1',
-            "dbuser" => 'ruben35',
-            "dbpass" => 'Ruben1986Hazenbosch35',
-            "dbname" => 'blueberry'
-        );
 
-//        $dbArray = array(
-//            "dbhost" => '172.21.159.197',
-//            "dbuser" => 'dbm',
-//            "dbpass" => 'em2XSp8AowGwC1t2',
-//            "dbname" => 'blueberry'
-//        );
+        public static function dbCredentials()
+        {
+            $dbArray = array(
+                "dbhost" => '127.0.0.1',
+                "dbuser" => 'ruben35',
+                "dbpass" => 'Ruben1986Hazenbosch35',
+                "dbname" => 'blueberry'
+            );
+            return( $dbArray );
+        }
 
-        return( $dbArray );
     }
 
-
-}
+endif;
